2bca9fb12c66c15818e8ab7a1986480c0697ffd2
bhusain <35381201+bhusain@users.noreply.github.com>
2019-06-28 12:27:51 -0700

    Update GEM_binning.py
    Fixed bin number error

--- GEM_binning.py
+++ GEM_binning.py
@@ -45,7 +45,7 @@ for i in range(0, len(data)):
                         ind = int((round(l) * bins) + round(m))
                         #print(ind)
                         k[ind] = k[ind] + 1
-                test = np.reshape(k, (10,10))
+                test = np.reshape(k, (bins,bins))
                 plt.imshow(test)
                 plt.show()
                 print(i, j)
